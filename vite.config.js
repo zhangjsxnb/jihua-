08f3d659956881802c065761b31a315d088f3165
zhangjsxnb <2053478992@qq.com>
2026-05-05 18:55:33 +0800

    12

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,39 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
-  base: './',})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Jihua Planner',
+        short_name: 'Planner',
+        description: '我的专属高颜值云端计划表',
+        theme_color: '#FFFBF8',
+        background_color: '#FFFBF8',
+        display: 'standalone', // 关键：让 App 全屏运行，隐藏浏览器地址栏
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ]
+})
